60c494421830ddfa024b56acbaf15e2295a16652
vladimir <v.vechkanov@profsoft.pro>
2018-03-15 15:35:34 +0400

    Initial commit

---
+++ app/src/main/java/com/school/vladimir/schoolapp/MainActivity.java
@@ -0,0 +1,38 @@
+package com.school.vladimir.schoolapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        textView = findViewById(R.id.text);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doAction();
+            }
+        });
+    }
+
+    void printToast(String text) {
+        textView.setText(text);
+    }
+
+    void doAction() {
+        printToast("Action");
+    }
+}
